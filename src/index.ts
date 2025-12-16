19fe3936dbdcd95c9a087bd454149a7bb29f490e
Lim Teik Yee <teikyee.lim@it.destijltech.com>
2025-12-16 16:13:52 +0800

    add elysia

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-export * from '@/isOdd' // absolute path
-export * from './isEven' // relative path
+import { Elysia } from 'elysia'
+import { node } from '@elysiajs/node'
+
+new Elysia({ adapter: node() })
+	.post('/event', () => {})
+	.listen(3000, ({ hostname, port }) => {
+		console.log(`🦊 Elysia is running at ${hostname}:${port}`)
+	})
