1ae20d79e7c9f9e24fa0ebc8baf912b347578664
tylim <limteikyee@gmail.com>
2025-12-22 08:22:03 +0800

    update drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
 	dialect: 'postgresql',
-	schema: './src/db/schema.ts',
+	schema: './src/db/driver.location.ts',
 	out: './drizzle',
 	dbCredentials: {
 		url: process.env.DATABASE_URL || '',
