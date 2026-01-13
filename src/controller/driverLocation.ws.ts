078b27d23d1d6e6cb62af0d3c841811d2031ee1b
Lim Teik Yee <teikyee.lim@it.destijltech.com>
2026-01-13 15:24:49 +0800

    remove redundant ws deletion logic

--- src/controller/driverLocation.ws.ts
+++ src/controller/driverLocation.ws.ts
@@ -69,16 +69,11 @@ export const driverLocation_ws = () => {
 			} else {
 				console.error('no related subscribers')
 			}
-			// remove single ws instance when ws connection close
-			if (!subscribers.get(driver_id)?.delete(ws))
-				console.error('warning, delete ws unsuccessful, may cause memory leak')
-
 			// if no users listening to specific driver, remove all ws instance
-			if (!subscribers.get(driver_id)?.size && !subscribers.delete(driver_id)) {
+			if (!subscribers.get(driver_id)?.size && !subscribers.delete(driver_id))
 				console.error(
 					`warning, delete all ${driver_id} ws unsuccessful, may cause memory leak`,
 				)
-			}
 		},
 	})
 
